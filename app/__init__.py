ead7dc5d0020ad1ef35b6960cb24c6d320524607
Daniel Barreto <dbarreto@talpor.com>
2013-04-28 14:34:37 -0430

    fixing the same problem as before but with static content.

--- app/__init__.py
+++ app/__init__.py
@@ -15,10 +15,11 @@ DATABASE_URL = 'postgresql://futbolito:ttaallppoorr@/futbolito'
 SECRET_KEY = 'A0Zr98j/3yXsdr R~XHXFG!jmN]ASSR/,?RT'
 SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
-template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                           '../templates')
+root_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
 
-app = Flask(__name__, template_folder=template_dir)
+app = Flask(__name__,
+            static_folder=os.path.join(root_dir, 'static'),
+            template_folder=os.path.join(root_dir, 'templates'))
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 db.init_app(app)
